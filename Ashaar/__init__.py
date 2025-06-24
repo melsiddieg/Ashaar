d7f063bc58ff5aba8c82201d48c71f386efab5fd
Mohammed OE Abdallah <melsiddieg@users.noreply.github.com>
2025-06-24 13:43:20 -0700

    Add package initializer

---
+++ Ashaar/__init__.py
@@ -0,0 +1,11 @@
+"""Top level package for Ashaar."""
+
+from .bait_analysis import BaitAnalysis
+from . import models
+from . import utils
+
+__all__ = [
+    "BaitAnalysis",
+    "models",
+    "utils",
+]
